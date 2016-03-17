d864aca10823c60a5bad8fedce036b2099a656b7
Howard Canaway <howard.canaway@gmail.com>
2016-03-17 13:00:01 -0400

    Slices
    demos array access, calling them slices reminds me of python

---
+++ slices.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	primes := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("primes ==", primes)
+
+	for i := 0; i < len(primes); i++ {
+		fmt.Printf("primes[%d] == %d\n", i, primes[i])
+	}
+}
