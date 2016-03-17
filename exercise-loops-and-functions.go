868311d569d8bc59ad81ab57f89dad137ae7e711
Howard Canaway <howard.canaway@gmail.com>
2016-03-17 11:15:12 -0400

    Exercise: Loops and Functions
    a quick implimentation of newton's methods, it seems to work well enough! I'm only iterating 10 times.

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,6 +5,12 @@ import (
 )
 
 func Sqrt(x float64) float64 {
+	z := float64(1) // from the hint on the tour site
+	for i := 0; i < 10; i++ {
+		z = z - ((z*z - x) / (2 * z))
+	}
+
+	return z
 }
 
 func main() {
